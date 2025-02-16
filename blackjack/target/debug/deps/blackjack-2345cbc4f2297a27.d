3a7c41cb1c737d8615a766a4ecf53aa5c67761bb
HazzMattX <dowdmatt93@gmail.com>
2025-02-16 10:45:25 -0800

    Initialize Blackjack project with basic structure and main function

---
+++ blackjack/target/debug/deps/blackjack-2345cbc4f2297a27.d
@@ -0,0 +1,5 @@
+/Users/matthewdowd/Documents/GitHub/rust-projects/blackjack/target/debug/deps/libblackjack-2345cbc4f2297a27.rmeta: src/main.rs
+
+/Users/matthewdowd/Documents/GitHub/rust-projects/blackjack/target/debug/deps/blackjack-2345cbc4f2297a27.d: src/main.rs
+
+src/main.rs:
---
+++ blackjack/target/debug/deps/blackjack-2c422a64fcc5e138.d
@@ -0,0 +1,5 @@
+/Users/matthewdowd/Documents/GitHub/rust-projects/blackjack/target/debug/deps/libblackjack-2c422a64fcc5e138.rmeta: src/main.rs
+
+/Users/matthewdowd/Documents/GitHub/rust-projects/blackjack/target/debug/deps/blackjack-2c422a64fcc5e138.d: src/main.rs
+
+src/main.rs:
