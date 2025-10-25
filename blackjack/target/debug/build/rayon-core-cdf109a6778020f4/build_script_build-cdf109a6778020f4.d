344fef1b24aea449c84f5caacd732c4cc0c8059d
HazzMattX <dowdmatt93@gmail.com>
2025-10-25 10:27:22 -0700

    Add build artifacts for rand_core and unicode_ident dependencies
    - Generated dependency files for rand_core version 0.6.4.
    - Generated dependency files for unicode-ident version 1.0.16.

---
+++ blackjack/target/debug/build/rayon-core-cdf109a6778020f4/build_script_build-cdf109a6778020f4.d
@@ -0,0 +1,5 @@
+C:\Users\dowdm\Documents\GitHub\rust-projects\blackjack\target\debug\build\rayon-core-cdf109a6778020f4\build_script_build-cdf109a6778020f4.d: C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rayon-core-1.12.1\build.rs
+
+C:\Users\dowdm\Documents\GitHub\rust-projects\blackjack\target\debug\build\rayon-core-cdf109a6778020f4\build_script_build-cdf109a6778020f4.exe: C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rayon-core-1.12.1\build.rs
+
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rayon-core-1.12.1\build.rs:
---
+++ blackjack/target/debug/deps/rand_core-ec59a8da42d28a33.d
@@ -0,0 +1,10 @@
+C:\Users\dowdm\Documents\GitHub\rust-projects\blackjack\target\debug\deps\rand_core-ec59a8da42d28a33.d: C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\lib.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\block.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\error.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\impls.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\le.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\os.rs
+
+C:\Users\dowdm\Documents\GitHub\rust-projects\blackjack\target\debug\deps\librand_core-ec59a8da42d28a33.rmeta: C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\lib.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\block.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\error.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\impls.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\le.rs C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\os.rs
+
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\lib.rs:
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\block.rs:
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\error.rs:
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\impls.rs:
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\le.rs:
+C:\Users\dowdm\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\rand_core-0.6.4\src\os.rs:
